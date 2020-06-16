c192b1fda00d9d87a8252f390f2ebb5a5ea5d23c
Nguyen Duc Huy B <nguyen.duc.huyb@sun-asterisk.com>
2020-06-16 15:41:36 +0700

    Create Autoscroll CollectionView

--- AutoscrollCollectionView/AutoscrollCollectionView/ViewController.swift
+++ AutoscrollCollectionView/AutoscrollCollectionView/ViewController.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
+let kCollectionViewNumberOfSets: Int = 4
+let kCollectionViewLineSpacing: CGFloat = 4.0
+let kNumberOfBanners: Int = 6
+let kMaxAutoScrollSpeed: CGFloat = 100
+let kMinAutoScrollSpeed: CGFloat = 0
+let kCentimeterOf1Inch: CGFloat = 2.54
+let kAutoScrollDefaultTimerInterval: CGFloat = 0.01
+
 final class ViewController: UIViewController {
     @IBOutlet private weak var dishesCollectionView: InfinityCollectionView!
+    
     private var bannerItems: [String] = []
-    private let kCollectionViewNumberOfSets: Int = 4
-    private let kCollectionViewLineSpacing: CGFloat = 4.0
-    private let kNumberOfBanners: Int = 6
+    private var autoScrollSpeed: CGFloat! // mm/s
+    private var timerInterval: CGFloat!
+    private var movePointAmountForTimerInterval: CGFloat!
+    private var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,11 +31,12 @@ final class ViewController: UIViewController {
     }
 
     private func config() {
-        
+        // Initial Data
         for n in 1...kNumberOfBanners {
             bannerItems.append("login_scroller_\(n)_Normal")
         }
         
+        // CollectionView's Layout
         let layout = UICollectionViewFlowLayout()
         layout.minimumLineSpacing = kCollectionViewLineSpacing
         layout.scrollDirection = .horizontal
@@ -33,11 +44,70 @@ final class ViewController: UIViewController {
         layout.itemSize = CGSize(width: widthContent, height: widthContent)
         dishesCollectionView.collectionViewLayout = layout
         
+        // CollectionView's Settings
         dishesCollectionView.dataSource = self
         dishesCollectionView.showsVerticalScrollIndicator = false
         dishesCollectionView.showsHorizontalScrollIndicator = false
         dishesCollectionView.scrollsToTop = false
         dishesCollectionView.numberOfSets = kCollectionViewNumberOfSets
+        
+        setAutoScrollSpeed(30)
+    }
+    
+    private func setAutoScrollSpeed(_ autoScrollSpeed: CGFloat) {
+        var availableAutoScrollSpeed: CGFloat
+        if autoScrollSpeed > kMaxAutoScrollSpeed {
+            availableAutoScrollSpeed = kMaxAutoScrollSpeed
+        } else if autoScrollSpeed < kMinAutoScrollSpeed {
+            availableAutoScrollSpeed = kMinAutoScrollSpeed
+        } else {
+            availableAutoScrollSpeed = autoScrollSpeed
+        }
+
+        self.autoScrollSpeed = availableAutoScrollSpeed
+        if (self.autoScrollSpeed > 0) {
+            let movePixelAmountForOneSeconds = self.autoScrollSpeed * CGFloat(DeviceInfo.getPixelPerInch()) * 0.1 / kCentimeterOf1Inch
+            let movePointForOneSeconds = movePixelAmountForOneSeconds / UIScreen.main.scale
+            let movePointAmountForTimerInterval = movePointForOneSeconds * kAutoScrollDefaultTimerInterval
+            let floorMovePointAmountForTimerInterval = floor(movePointAmountForTimerInterval)
+            if floorMovePointAmountForTimerInterval < 1 {
+                self.movePointAmountForTimerInterval = 1
+            } else {
+                self.movePointAmountForTimerInterval = floorMovePointAmountForTimerInterval
+            }
+            self.timerInterval = self.movePointAmountForTimerInterval / movePointForOneSeconds
+        }
+        
+        startAutoScroll()
+    }
+    
+    private func startAutoScroll() {
+        if self.timer.isValid {
+            return
+        }
+        
+        if self.autoScrollSpeed == 0 {
+            self.stopAutoScroll()
+        } else {
+            self.timer = Timer.scheduledTimer(timeInterval: TimeInterval(self.timerInterval),
+                                              target: self,
+                                              selector: #selector(timerDidFire),
+                                              userInfo: nil,
+                                              repeats: true)
+        }
+    }
+    
+    private func stopAutoScroll() {
+        if self.timer.isValid {
+            self.timer.invalidate()
+        }
+    }
+    
+    @objc
+    private func timerDidFire() {
+        let nextContentOffset = CGPoint(x: self.dishesCollectionView.contentOffset.x + self.movePointAmountForTimerInterval,
+                                        y: self.dishesCollectionView.contentOffset.y)
+        self.dishesCollectionView.contentOffset = nextContentOffset
     }
 }
 
